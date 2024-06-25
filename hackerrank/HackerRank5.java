95660fb21aeff1c40df7218efef54bae914869ec
Github-Ganesh7 <saiganeshgogu@gmail.com>
2024-06-25 11:51:52 +0530

    hakerrank folder

---
+++ hackerrank/HackerRank5.java
@@ -0,0 +1,22 @@
+//java 100
+//cpp 65
+//python 50
+
+
+import java.util.*; 
+
+class HackerRank5
+{
+	public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-15s%03d%n",s1,x);
+            }
+            System.out.println("================================");
+
+    }
+}
